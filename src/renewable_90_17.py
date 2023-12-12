3539688cb8ce780f8974d8d5f4aa5cbb9298d84a
Andrea Zampedri <92220364+andrzamp33@users.noreply.github.com>
2023-12-12 09:19:38 +0100

    Update renewable_90_17.py

--- src/renewable_90_17.py
+++ src/renewable_90_17.py
@@ -1,77 +1,67 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-from io import StringIO
+import matplotx
 
-# Leggi i dati
-data = """
-Year,Hydro(TWh),Biofuel(TWh),Solar PV (TWh),Geothermal (TWh)
-1990,2191.67,3.88,0.09,36.42
-1991,2268.63,4.19,0.1,37.39
-1992,2267.16,4.63,0.12,39.3
-1993,2397.67,5.61,0.15,40.23
-1994,2419.73,7.31,0.17,41.05
-1995,2545.96,7.95,0.19,39.89
-1996,2583.18,9.45,0.22,42.18
-1997,2614.54,12.08,0.27,42.38
-1998,2628.63,16.07,0.35,45.35
-1999,2636.26,21.52,0.61,48.66
-2000,2695.85,31.34,0.99,51.98
-2001,2638.2,38.45,1.32,51.57
-2002,2711.12,52.85,1.58,52.29
-2003,2726.33,64.23,2.01,54.09
-2004,2894.22,84.43,2.66,56.5
-2005,3019.5,103.92,3.92,58.28
-2006,3124.34,133.05,5.52,59.61
-2007,3165.71,170.83,7.47,62.29
-2008,3285.59,221.05,11.92,64.91
-2009,3338.82,277.44,20.04,67.03
-2010,3530.72,341.38,32.22,68.1
-2011,2603.78,436.01,63.76,69.29
-2012,3765.96,525.04,99.03,70.27
-2013,2898.28,646.36,139.64,71.74
-2014,3976.01,718.07,190.25,77.52
-2015,3989.28,838.31,250.75,80.56
-2016,4162.26,958.15,329.14,82.18
-2017,4197.29,1127.31,443.55,85.34
-"""
+# Codice 1: Grafico delle energie rinnovabili
+df_renewable = pd.read_csv("renewable_en.csv")
 
-# Leggi i dati dallo string io
-df = pd.read_csv(StringIO(data))
+years_renewable = df_renewable['Year']
+hydro = df_renewable['Hydro(TWh)']
+biofuel = df_renewable['Biofuel(TWh)']
+solar_pv = df_renewable['Solar PV (TWh)']
+geothermal = df_renewable['Geothermal (TWh)']
 
-# Estrai i dati
-years = df['Year']
-hydro = df['Hydro(TWh)']
-biofuel = df['Biofuel(TWh)']
-solar_pv = df['Solar PV (TWh)']
-geothermal = df['Geothermal (TWh)']
-
-# Attiva lo stile 
 plt.style.use("default")
+fig, axs = plt.subplots(1, 2, figsize=(15, 6), sharey=True)
 
-# Plotting
-plt.figure(figsize=(10, 6))
+colors_renewable = ["#aec6cf", "#c9e2b3", "#ffeaa7", "#f8c471"]
+size = 1.5
+axs[0].plot(years_renewable, hydro, label='Hydro', color=colors_renewable[0], linewidth=size)
+axs[0].plot(years_renewable, biofuel, label='Biofuel', color=colors_renewable[1], linewidth=size)
+axs[0].plot(years_renewable, solar_pv, label='Solar PV', color=colors_renewable[2], linewidth=size)
+axs[0].plot(years_renewable, geothermal, label='Geothermal', color=colors_renewable[3], linewidth=size)
 
-# Colori distinti per ciascuna linea
-colors = ["#aec6cf", "#c9e2b3", "#ffeaa7","#f8c471"]
-
-size = 4
-plt.plot(years, hydro, label='Hydro', marker='.', color=colors[0], linewidth= size)
-plt.plot(years, biofuel, label='Biofuel', marker='.', color=colors[1], linewidth= size)
-plt.plot(years, solar_pv, label='Solar PV', marker='.', color=colors[2], linewidth= size)
-plt.plot(years, geothermal, label='Geothermal', marker='.', color=colors[3], linewidth= size)
-
-# Aggiungi etichette e titoli al grafico
-plt.xlabel('', fontsize=18)
-plt.ylabel('Energy Generation (TWh)', fontsize=16)
-plt.title('Renewable Power Generation (1990-2017)', fontsize=20)
-plt.legend()
-
-# Rimuovi griglie
-plt.grid(False)
-
-# Rimuovi i bordi
+axs[0].set_xlabel('')
+axs[0].set_ylabel('Energy generation (TWh)')
+axs[0].set_title('World data on renewable power generation (1990-2017)')
+matplotx.line_labels()
+axs[0].grid(False)
+axs[0].legend()
 sns.despine()
 
-# Mostra il grafico
+# Codice 2: Grafico dei combustibili fossili
+coal_data = pd.read_csv("coal-consumption-by-country-terawatt-hours-twh.csv", usecols=["Entity", "Year", "Coal consumption - TWh"]).rename(columns={"Coal consumption - TWh": "Consumption"})
+fossil_data = pd.read_csv("fossil-fuel-primary-energy.csv", usecols=["Entity", "Year", "Fossil fuels (TWh)"]).rename(columns={"Fossil fuels (TWh)": "Consumption"})
+oil_data = pd.read_csv("oil-consumption-by-country.csv", usecols=["Entity", "Year", "Oil consumption - TWh"]).rename(columns={"Oil consumption - TWh": "Consumption"})
+gas_data = pd.read_csv("gas-consumption-by-country.csv", usecols=["Entity", "Year", "Gas consumption - TWh"]).rename(columns={"Gas consumption - TWh": "Consumption"})
+carbon_data = pd.read_csv("low-carbon-energy-consumption.csv", usecols=["Entity", "Year", "Low-carbon energy (TWh - equivalent)"]).rename(columns={"Low-carbon energy (TWh - equivalent)": "Consumption"})
+nuclear_data = pd.read_csv("primary-energy-nuclear.csv", usecols=["Entity", "Year", "Nuclear (TWh - equivalent)"]).rename(columns={"Nuclear (TWh - equivalent)": "Consumption"})
+
+data = [coal_data, fossil_data, oil_data, gas_data, carbon_data, nuclear_data]
+names = ["Coal", "Fossil fuel", "Oil", "Gas", "Low carbon", "Nuclear"]
+colors = ['#FFBB99', '#935C7F', '#CDAAB4', '#B85381', '#FFD699', '#FF8B8E']
+
+filtered_data = []
+
+for df in data:
+    filtered_df = df[(df['Entity'] == 'World') & (df['Year'] >= 1990) & (df['Year'] <= 2017)]
+    filtered_data.append(filtered_df)
+
+i = 0
+for fd in filtered_data:
+    axs[1].plot(fd["Year"].astype(int), fd["Consumption"], color=colors[i], label=names[i], linewidth=size)
+    i += 1
+
+matplotx.line_labels()
+
+axs[1].set_ylabel("Energy consumption (TWh)")
+axs[1].set_title("World data on non-renewable power generation (1990-2017)")
+
+for ax in axs:
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+
+plt.subplots_adjust(wspace=0.4)
+plt.savefig("combined_plot.png")
 plt.show()
